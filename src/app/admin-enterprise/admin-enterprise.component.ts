2c412d3e4fdb0659a26826f2b08938490a140886
Kathy Flores <jflores@ec.krugercorp.com>
2022-11-21 00:34:34 -0500

    prueba servicio

--- src/app/admin-enterprise/admin-enterprise.component.ts
+++ src/app/admin-enterprise/admin-enterprise.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import EnterpriseVO from '../model/EnterpriseVO';
+import { AppMgmtService } from '../service/app-mgmt.service';
 
 @Component({
   selector: 'app-admin-enterprise',
   templateUrl: './admin-enterprise.component.html',
   styleUrls: ['./admin-enterprise.component.css']
 })
+
 export class AdminEnterpriseComponent implements OnInit {
 
-  constructor() { }
+  enterpriseAll: EnterpriseVO[]= [];
+  constructor(private appMgmtService:AppMgmtService) { }
+
 
   ngOnInit(): void {
+    this.getAllEnterprises();
   }
 
+
+  getAllEnterprises(){
+    this.appMgmtService.getAllEnterprises().subscribe(resp =>{
+      this.enterpriseAll=resp;
+      console.warn('ENTERPRISES ALL',this.enterpriseAll);
+    })
+}
 }
