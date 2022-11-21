a30586c86accfd5dd5057a5c48d3402584e568cf
Kathy Flores <jflores@ec.krugercorp.com>
2022-11-21 07:33:30 -0500

    Se agrega estilo y se crea el componente dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { AdminEnterpriseComponent } from './admin-enterprise/admin-enterprise.component';
 import { AdminDepartmentComponent } from './admin-department/admin-department.component';
 import { AdminEmployeeComponent } from './admin-employee/admin-employee.component';
+
 import { HttpClientModule } from '@angular/common/http';
+
+import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +22,16 @@ import { HttpClientModule } from '@angular/common/http';
     NavigationComponent,
     AdminEnterpriseComponent,
     AdminDepartmentComponent,
-    AdminEmployeeComponent
+    AdminEmployeeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MenubarModule,
+    InputTextModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { constants } from '../constants/constants';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-navigation',
@@ -6,10 +8,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
+  menuList: MenuItem[]=[];
+  nav:any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.nav=0;
+    this.menuList = [
+      {
+          label:constants.VIEWS.HOME,
+          icon:'pi pi-fw pi-file',
+          command: () => {
+            this.goDashboard();
+        }
+      },
+      {
+          label:constants.VIEWS.ENTERPRISES,
+          icon:'pi pi-fw pi-pencil',
+          command: () => {
+            this.goEnterprises();
+        }
+
+      },
+      {
+          label:constants.VIEWS.DEPARTMENTS,
+          icon:'pi pi-fw pi-user',
+          command: () => {
+            this.goDepartments();
+        }
+      },
+      {
+          label:constants.VIEWS.EMPLOYEES,
+          icon:'pi pi-fw pi-power-off',
+          command: () => {
+            this.goEmployees();
+        }
+      }
+  ];
+  }
+  goEmployees() {
+    this.nav=3;
+  }
+  goDepartments() {
+    this.nav=2;
+  }
+  goEnterprises() {
+    this.nav=1;
+  }
+
+  goDashboard() {
+    this.nav=0;
   }
 
 }
